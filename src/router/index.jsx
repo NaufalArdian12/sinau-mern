41961c5dd50a7c3cc41c0dc8fac2363f6bb84690
NaufalArdian12 <naufalopo87@gmail.com>
2025-07-08 19:49:25 +0700

    feat: add sign up view

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import ManagerHome from "../pages/ManagerHome";
 import SignInPage from "../pages/SignIn";
+import SignUpPage from "../pages/SignUp";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     path: "/manager/sign-in",
     element: <SignInPage />,
   },
-])
+  {
+    path: "/manager/sign-up",
+    element: <SignUpPage />,
+  },
+]);
 
 export default router;
