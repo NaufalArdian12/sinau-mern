d69b3d8cccd9326028aff2d537499e151ae172a4
NaufalArdian12 <naufalopo87@gmail.com>
2025-07-08 15:20:38 +0700

    feat: add sign in view

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import ManagerHome from "../pages/ManagerHome";
+import SignInPage from "../pages/SignIn";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <ManagerHome />,
-  }
+  },
+  {
+    path: "/manager/sign-in",
+    element: <SignInPage />,
+  },
 ])
 
 export default router;
