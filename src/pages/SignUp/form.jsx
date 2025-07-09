28a1f16124291c6435303e8266bc13e81cccdb85
NaufalArdian12 <naufalopo87@gmail.com>
2025-07-09 21:40:22 +0700

    fix: navbar and button

--- src/pages/SignUp/form.jsx
+++ src/pages/SignUp/form.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Navbar from '../../components/navbar'
+import { Link } from 'react-router-dom'
 
 export default function Form() {
   return (
@@ -7,34 +9,18 @@ export default function Form() {
             <img src="/assets/images/backgrounds/background-glow.png" className="absolute bottom-0 transform -translate-x-1/2 left-1/2" alt="" />
         </div>
         <nav className="flex items-center justify-between p-[30px]">
-            <div className="flex items-center gap-[60px]">
-                <img src="/assets/images/logos/logo.svg" className="flex shrink-0" alt="logo" />
-                <ul className="flex items-center gap-10">
-                    <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-                        <a href="#">Home</a>
-                    </li>
-                    <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-                        <a href="pricing.html">Pricing</a>
-                    </li>
-                    <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-                        <a href="#">Features</a>
-                    </li>
-                    <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-                        <a href="#">Testimonials</a>
-                    </li>
-                </ul>
-            </div>
+            <Navbar />
             <div className="flex items-center gap-3">
-                <a href="signin.html" >
+                <Link to="" >
                     <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
                         <span className="font-semibold text-white">My Dashboard</span>
                     </div>
-                </a>           
-                <a href="signup.html" >
+                </Link>           
+                <Link to="/manager/sign-in" >
                     <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#662FFF] border-[#8661EE] shadow-[-10px_-6px_10px_0_#7F33FF_inset]">
-                        <span className="font-semibold text-white">Sign Up</span>
+                        <span className="font-semibold text-white">Sign In</span>
                     </div>
-                </a>
+                </Link>
             </div>
         </nav>
         <div className="flex items-center justify-center gap-[109px] my-auto mt-20" >
