65218d6a4d78b33a75c304f5bee70e91e71ae12d
NaufalArdian12 <naufalopo87@gmail.com>
2025-08-02 00:29:09 +0700

    feat: add outlet

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Sidebar from "./sidebar";
 import Header from "./header";
 import Courses from "../pages/ManagerHome/latest-courses";
@@ -12,31 +12,7 @@ export default function LayoutDashboard() {
       <Sidebar />
       <main className="flex flex-col flex-1 gap-[30px] p-[30px] ml-[290px]">
         <Header />
-        <header className="flex items-center justify-between gap-[30px]">
-          <div>
-            <h1 className="font-extrabold text-[28px] leading-[42px]">
-              Overview
-            </h1>
-            <p className="text-[#838C9D] mt-[1]">Grow your company quickly</p>
-          </div>
-          <div className="flex items-center gap-3">
-            <Link
-              to="#"
-              className="w-fit rounded-full border border-[#060A23] p-[14px_20px] font-semibold text-nowrap">
-              Customize
-            </Link>
-            <Link
-              to=""
-              className="w-fit rounded-full p-[14px_20px] font-semibold text-[#FFFFFF] bg-[#662FFF] text-nowrap">
-              Export Data
-            </Link>
-          </div>
-        </header>
-        <Stats />
-        <div className="grid grid-cols-2 gap-[30px]">
-          <Courses />
-          <Students />
-        </div>
+        <Outlet />
       </main>
     </div>
   );
--- src/pages/ManagerHome/index.jsx
+++ src/pages/ManagerHome/index.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { Link, Outlet } from "react-router-dom";
+import Courses from './latest-courses'
+import Students from './latest-students'
+import Stats from '../../components/stats'
 
 export default function ManagerHome() {
   return (
-    <div>ManagerHome</div>
+    <>
+      <header className="flex items-center justify-between gap-[30px]">
+          <div>
+            <h1 className="font-extrabold text-[28px] leading-[42px]">
+              Overview
+            </h1>
+            <p className="text-[#838C9D] mt-[1]">Grow your company quickly</p>
+          </div>
+          <div className="flex items-center gap-3">
+            <Link
+              to="#"
+              className="w-fit rounded-full border border-[#060A23] p-[14px_20px] font-semibold text-nowrap">
+              Customize
+            </Link>
+            <Link
+              to=""
+              className="w-fit rounded-full p-[14px_20px] font-semibold text-[#FFFFFF] bg-[#662FFF] text-nowrap">
+              Export Data
+            </Link>
+          </div>
+        </header>
+        <Stats />
+        <div className="grid grid-cols-2 gap-[30px]">
+          <Courses />
+          <Students />
+        </div>
+    </>
   )
 }
