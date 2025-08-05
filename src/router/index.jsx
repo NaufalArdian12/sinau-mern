fc9f2435d7fe277b5bb34c660d69d1fea7cd139b
NaufalArdian12 <naufalopo87@gmail.com>
2025-08-06 00:14:23 +0700

    feat: add path for detail courses page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -6,6 +6,7 @@ import SuccessCheckoutPage from "../pages/SuccessCheckout";
 import LayoutDashboard from "../components/layout";
 import ManageCoursePage from "../pages/Manager/courses";
 import ManageCreateCoursePage from "../pages/Manager/create-courses";
+import ManagerCourseDetailPage from "../pages/Manager/course-detail";
 
 const router = createBrowserRouter([
   {
@@ -39,6 +40,10 @@ const router = createBrowserRouter([
       {
         path: "/manager/courses/create",
         element: <ManageCreateCoursePage />,
+      },
+      {
+        path: "/manager/courses/:id",
+        element: <ManagerCourseDetailPage/>,
       },
     ],
   },
