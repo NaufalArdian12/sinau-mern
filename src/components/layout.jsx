59cc254a92ad2e523225dc80272b112885b04e1b
NaufalArdian12 <naufalopo87@gmail.com>
2025-08-07 23:51:54 +0700

    feat: add usematch from router dom

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useMatch } from "react-router-dom";
 import Sidebar from "./sidebar";
 import Header from "./header";
-import Courses from "../pages/Manager/home/latest-courses";
-import Students from "../pages/Manager/home/latest-students";
-import Stats from "./stats";
 
 export default function LayoutDashboard() {
+  const ispreviewPage = useMatch("/manager/courses/:id/preview");
+
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <main className="flex flex-col flex-1 gap-[30px] p-[30px] ml-[290px]">
-        <Header />
+    <>
+      {!ispreviewPage !== null ? (
         <Outlet />
-      </main>
-    </div>
+      ) : (
+        <div className="flex min-h-screen">
+          <Sidebar />
+          <main className="flex flex-col flex-1 gap-[30px] p-[30px] ml-[290px]">
+            <Header />
+            <Outlet />
+          </main>
+        </div>
+      )}
+    </>
   );
 }
