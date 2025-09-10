e5d85c0bac985a9b9fce02a091e56c0bfda1c6d4
NaufalArdian12 <naufalopo87@gmail.com>
2025-09-10 22:08:01 +0700

    feat: add course model

---
+++ backend/src/models/courseModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const courseModel = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    tagline: {
+      type: String,
+      required: true,
+    },
+    students: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    manager: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+export default mongoose.model("Course", courseModel);
