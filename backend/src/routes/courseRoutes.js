d3e648bc6960ee5ae2c73bd67590b02c30a7b9b4
NaufalArdian12 <naufalopo87@gmail.com>
2025-09-13 23:02:32 +0700

    feat: add multer into course routes

--- backend/src/routes/courseRoutes.js
+++ backend/src/routes/courseRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { getCourses } from "../controllers/courseController.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
+import { fileFilter } from "../utils/multer.js";
+import multer from "multer";
+import { fileStorageCourse } from "../utils/multer.js";
+import { postCourse } from "../controllers/courseController.js";
 
 const courseRoutes = express.Router();
 
+const upload = multer({
+  storage: fileStorageCourse,
+  fileFilter: fileFilter,
+});
+
 courseRoutes.get("/courses", verifyToken, getCourses);
+courseRoutes.post("/courses", verifyToken, upload.single("thumbnail"), postCourse);
 
 export default courseRoutes;
