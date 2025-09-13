1190ca29ab8a631263b0a736929ad80413106b04
NaufalArdian12 <naufalopo87@gmail.com>
2025-09-13 23:01:56 +0700

    feat: add post course controller

--- backend/src/controllers/courseController.js
+++ backend/src/controllers/courseController.js
@@ -1,4 +1,8 @@
 import courseModel from "../models/courseModel.js";
+import fs from "fs";
+import { mutateCourseSchema } from "../utils/schema.js";
+import categoryModel from "../models/categoryModel.js";
+import userModel from "../models/userModel.js";
 
 export const getCourses = async (req, res) => {
   try {
@@ -19,6 +23,60 @@ export const getCourses = async (req, res) => {
       message: "Get courses successfully",
       data: courses,
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+export const postCourse = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const parse = mutateCourseSchema.safeParse(body);
+
+    if (!parse.success) {
+      const errorMessages = parse.error.errors.map((err) => err.message);
+
+      if (req?.file?.path && fs.existsSync(req?.file?.path)) {
+        fs.unlinkSync(req?.file?.path);
+      }
+      return res.status(400).json({ error: errorMessages });
+    }
+
+    const category = await categoryModel.findById(parse.data.categoryId);
+    if (!category) {
+      return res.status(400).json({ error: "Category not found" });
+    }
+
+    const course = new courseModel({
+      name: parse.data.name,
+      category: category._id,
+      tagline: parse.data.tagline,
+      description: parse.data.description,
+      thumbnail: req.file?.filename,
+      manager: req.user._id,
+    });
+
+    await course.save();
+
+    await categoryModel.findByIdAndUpdate(
+      category._id,
+      {
+        $push: { courses: course._id },
+      },
+      { new: true }
+    );
+
+    await userModel.findByIdAndUpdate(
+      req.user._id,
+      {
+        $push: { courses: course._id },
+      },
+      { new: true }
+    );
+
+    return res.json({ message: "Create course successfully", data: course });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Something went wrong" });
