24c07cbd689a3b377b4251c0cbaa9843feb905ec
NaufalArdian12 <naufalopo87@gmail.com>
2025-09-21 20:24:26 +0700

    feat: add start and build in package json

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,32 +1,31 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import globalRoutes  from "./routes/globalRoutes.js"
-import authroutes from './routes/authRoutes.js'
-import connectDB from './utils/database.js'
-import paymentRoutes from './routes/paymentRoutes.js'
-import courseRoutes from './routes/courseRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import globalRoutes from "./routes/globalRoutes.js";
+import authroutes from "./routes/authRoutes.js";
+import connectDB from "./utils/database.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import courseRoutes from "./routes/courseRoutes.js";
 
-const app = express()
+const app = express();
+const express = require("express");
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const port = 3000
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(express.static('public'))
+app.get("/", (req, res) => {
+  res.send("Hello Worldss");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello Worldss')
-})
+app.use("/api", globalRoutes);
+app.use("/api", paymentRoutes);
+app.use("/api", authroutes);
+app.use("/api", courseRoutes);
 
-app.use("/api", globalRoutes)
-app.use("/api", paymentRoutes)
-app.use("/api", authroutes)
-app.use("/api", courseRoutes)
-
-export default app
+module.exports = app;
