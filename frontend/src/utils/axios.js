b8c46acc88f3e171e12f9cf5d442fa142df1e599
NaufalArdian12 <naufalopo87@gmail.com>
2025-09-26 08:50:38 +0700

    feat: add apiInstanceToken

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,11 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_URL;
 
 const apiInstance = axios.create({
   baseURL,
   timeout: 3000,
+});
 
+export const apiInstanceToken = axios.create({
+  baseURL,
+  timeout: 3000,
+});
+
+apiInstanceToken.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `JWT ${token.token}`;
+  }
+  return config;
 });
 
 export default apiInstance;
